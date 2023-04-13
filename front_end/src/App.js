fd819157494f034a263dc36ab113021971c75c61
Артемий Есипенко <artemon36555@gmail.com>
2023-04-14 00:01:09 +0500

    exp

---
+++ front_end/src/App.js
@@ -0,0 +1,15 @@
+import Sign__menu from './sign_in_classes/sign_in_menu';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Sign__menu/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ front_end/src/sign_in_classes/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Mytext_button (props){
+    
+    return(<input type='text'/>);
+}
+
+export default Mytext_button;
